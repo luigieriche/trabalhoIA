2cc12da0a6cd2be5b8ed38e20effc944a533e1f2
luigieriche <luiz_paschoeto@hotmail.com>
2016-08-02 17:41:43 -0300

    Função Fitness finalizada.

--- src/trabalhoia/AlgoritmoSimulatedAnnealing.java
+++ src/trabalhoia/AlgoritmoSimulatedAnnealing.java
@@ -37,7 +37,7 @@ public class AlgoritmoSimulatedAnnealing {
         int t0 = 10;
         int l0 = 20;
         Empresa vizinho = new Empresa();
-        int custoAtual, custoVizinho;
+        double custoAtual, custoVizinho;
         
         for (int k = 0; k<= l0; k++){  
             for (int l = 1; k )
@@ -81,6 +81,7 @@ public class AlgoritmoSimulatedAnnealing {
                 porcentagem_producao = n.nextInt(5);
                 break;
             case 3:// Vendeu tudo o que produziu & Valor do seu produto é menor
+                porcentagem_marketing = n.nextInt(5) * -1;
                 porcentagem_producao = n.nextInt(5);
                 porcentagem_marketing = n.nextInt(5);
                 break;
@@ -99,21 +100,29 @@ public class AlgoritmoSimulatedAnnealing {
         return empresa;
 }
     
-    public int calculaCusto(Empresa empresa,Empresa auxiliar, int criterio){
-        int custo = 0;
+    public double calculaCusto(Empresa empresa,Empresa auxiliar, int criterio){
+        double custo = 0;
+        double custoVizinho = 0;
+        double custoEmpresa = 0;
         
+        // Valor total do que foi produzido.
+        custoVizinho = auxiliar.qtd_prod * auxiliar.vlr_prod;
+        custoEmpresa = empresa.qtd_prod * empresa.vlr_prod;
+
             switch (criterio) {
             case 0:// Produziu mais do que vendeu & Valor do seu produto é maior 
-            
+                custo = (custo + (auxiliar.vlr_marke - empresa.vlr_marke));
+                custo = (custo + custoEmpresa - custoVizinho);
                 break;
             case 1:// Produziu mais do que vendeu & Valor do seu produto é menor
-              
+                 custo = empresa.qtd_prod * auxiliar.qtd_prod;
                 break;
             case 2:// Vendeu tudo o que produziu & Valor do seu produto é maior
-               
+                custo = auxiliar.qtd_prod - empresa.qtd_prod;
                 break;
             case 3:// Vendeu tudo o que produziu & Valor do seu produto é menor
-               
+               custo = (custo + (empresa.vlr_marke - auxiliar.vlr_marke));
+               custo = (custo + custoVizinho - custoEmpresa);
                 break;
 
             default:
